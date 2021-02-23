9b09378cbbda9412d8b8102fc5beef7e1299910e
moritzmn <muellernavarra@gmail.com>
2021-02-23 12:09:07 +0100

    changed intervalls for the tests

--- tests/testthat/test-TideCurve.R
+++ tests/testthat/test-TideCurve.R
@@ -1,15 +1,15 @@
 library(TideCurves)
 
-tc <- TideCurve(dataInput = tideObservation, asdate = "2015/06/01",
+tc <- TideCurve(dataInput = tideObservation, asdate = "2014/06/01",
           astime = "00:00:00",      aedate = "2015/12/31",
-          aetime = "23:30:00",      ssdate = "2015/12/01",
+          aetime = "23:30:00",      ssdate = "2015/01/01",
           sstime = "00:00:00",      sedate = "2015/12/31",
           setime = "23:30:00")
 
 tc_model <- BuildTC(dataInput = tideObservation,
-                    asdate = "2015/06/01", aedate = "2015/12/31",
+                    asdate = "2014/06/01", aedate = "2015/12/31",
                     astime = "00:00:00", aetime = "23:30:00")
-tc_m <- SynTC(tmodel = tc_model,  ssdate = "2015/12/01", sstime = "00:00:00" ,
+tc_m <- SynTC(tmodel = tc_model,  ssdate = "2015/01/01", sstime = "00:00:00" ,
               sedate = "2015/12/31", setime = "23:30:00")
 
 test_that("equal lunar synthesis", {
